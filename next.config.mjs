b9dd6e96c6257c0e8d6a48ef82cdf15f58381dd2
Ganipudi Geethesh <ganipudigeethesh@Ganipudis-MacBook-Air.local>
2026-02-08 11:02:46 +0530

    Fix: Change next.config.mjs to .js for env variables

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,7 @@
 // export default nextConfig;
 const nextConfig = {
   // Use the new property name
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  },
+  
   serverExternalPackages: ['mongoose', 'bcryptjs'],
 }
 
